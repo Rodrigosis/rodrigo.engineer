7c667b22661cb976d24b2645c222baf435f938a7
Rodrigosis <40663984+Rodrigosis@users.noreply.github.com>
2020-01-17 16:16:27 -0300

    arquitetura modificada

--- rodrigo_engineer.py
+++ rodrigo_engineer.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('../template.html')
+    return render_template('template.html', titulo_my_name='Rodrigo da Silva Souza')
 
 
 app.run()
