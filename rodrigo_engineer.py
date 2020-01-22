d434c9cd0ce23247979a2b35876f4887d0175105
Rodrigo <rodrigo.sis.s7@gmail.com>
2020-01-21 21:45:45 -0300

    novas rotas adicionadas ao site

--- rodrigo_engineer.py
+++ rodrigo_engineer.py
@@ -11,4 +11,34 @@ def index():
     return render_template('index.html', titulo_my_name='Rodrigo da Silva Souza', lista=topicos)
 
 
+@app.route('/sobre')
+def sobre():
+    return render_template('sobre.html')
+
+
+@app.route('/projetos')
+def projetos():
+    return render_template('projetos.html')
+
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
+
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+
+@app.route('/curriculo')
+def curriculo():
+    return render_template('cv.html')
+
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
+
+
 app.run(debug=True)
