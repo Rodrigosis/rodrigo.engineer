5b3d9e3cad83946d5b127fabacc201e8facbb601
Rodrigosis <40663984+Rodrigosis@users.noreply.github.com>
2020-02-06 14:09:05 -0300

    Menu retratil implementado

--- static/js/template.js
+++ static/js/template.js
@@ -1,6 +1,18 @@
 var veri = 1;
-var trigger = document.getElementById('menu-trigger').addEventListener("click",function(){
-    var menu = document.getElementById('menu-hidde');
+
+var trigger = document.getElementById('menu').addEventListener("click", function (){
+var menu = document.getElementById('sidebar');
+    if (veri == 1) {
+        menu.style.left = "0px";
+        veri = 0;
+    }else{
+        menu.style.left = "-100%";
+        veri = 1;
+    }
+})
+
+var trigger = document.getElementById('activate_menu').addEventListener("click", function (){
+var menu = document.getElementById('sidebar');
     if (veri == 1) {
         menu.style.left = "0px";
         veri = 0;
